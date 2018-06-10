6392f72388476da4baf907848d86ad726d349d66
erick98 <ericksalazar_g@hotmail.com>
2018-06-09 19:23:54 -0600

    ahi van las clases con lo del xml hecho lindo
    No se por que son 6 files si solo son 2 en el proyecto...

--- ProyectoProgramadoII_IIIPOO/src/XML/reader.java
+++ ProyectoProgramadoII_IIIPOO/src/XML/reader.java
@@ -12,12 +12,17 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+import Clases.Platillo;
+
 
 public class reader {
     
-    
-   private void XMLReader(){
+   
+   private Platillo[] XMLReader(){
+       Platillo[] listaPlatillos = new Platillo[2];//ACA ESTA LA LISTA 
        try {
+         
+         Platillo platillo = new Platillo();
          File inputFile = new File("C:\\users\\erick\\Desktop\\file.xml");//archivo de donde se va a leer el xml
          DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
          DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -27,6 +32,7 @@ public class reader {
          NodeList nList = doc.getElementsByTagName("Platillo");//se hace una lista de todos platillos
          System.out.println("----------------------------");
          
+         int cont = nList.getLength()-1;
          for (int temp = 0; temp < nList.getLength(); temp++) {//se recorre cada platillo
             Node nNode = nList.item(temp);
             System.out.println("\nCurrent Element :" + nNode.getNodeName());
@@ -34,27 +40,57 @@ public class reader {
             if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                Element eElement = (Element) nNode;
                System.out.println("Id del platillo : " 
-                  + eElement.getAttribute("id"));
+                  + eElement.getAttribute("id")); 
                System.out.println("nombre : " 
                   + eElement.getElementsByTagName("nombre").item(0).getTextContent());
+               platillo.setNombre(eElement.getElementsByTagName("nombre").item(0).getTextContent());
                System.out.println("descripcion : " 
                   + eElement.getElementsByTagName("descripcion").item(0).getTextContent());
+               platillo.setDescripcion(eElement.getElementsByTagName("descripcion").item(0).getTextContent());
                System.out.println("precio : " 
                   + eElement.getElementsByTagName("precio").item(0).getTextContent());
+               platillo.setPrecio(Double.parseDouble(eElement.getElementsByTagName("precio").item(0).getTextContent()));
                System.out.println("tipoPorcion : " 
                   + eElement.getElementsByTagName("tipoPorcion").item(0).getTextContent());
+               System.out.println("habilitado : " 
+                  + eElement.getElementsByTagName("Habilitado").item(0).getTextContent());
+               platillo.setHabilitado(Boolean.parseBoolean(eElement.getElementsByTagName("Habilitado").item(0).getTextContent()));
+               System.out.println("cantPedidos : " 
+                  + eElement.getElementsByTagName("CantPedidos").item(0).getTextContent());
+               platillo.setCantPedidos(Integer.parseInt(eElement.getElementsByTagName("CantPedidos").item(0).getTextContent()));
+               System.out.println("codClave : " 
+                  + eElement.getElementsByTagName("codClave").item(0).getTextContent());
+               platillo.setCodClave(eElement.getElementsByTagName("codClave").item(0).getTextContent());
+               System.out.println("cantidadPlatos : " 
+                  + eElement.getElementsByTagName("CantPlatos").item(0).getTextContent());
+               platillo.setCantidadPlatos(Integer.parseInt(eElement.getElementsByTagName("CantPlatos").item(0).getTextContent()));
+               
+               
+               listaPlatillos[temp] = platillo;
+               
+               
+               
             }
          }
          
+      
          
          
       } catch (Exception e) {
          e.printStackTrace();
-        }
+        
+        
+   }
+       return listaPlatillos;
    }
    
+   public static void main(String[] args) {
+       reader xml = new reader();
+       Platillo[] lplatillo = new Platillo[100];
+       lplatillo = xml.XMLReader();
+       
+   }
    
-//   public static void main(String[] args) {
 //
 //      try {
 //         File inputFile = new File("C:\\users\\erick\\Desktop\\file.xml");
--- ProyectoProgramadoII_IIIPOO/src/XML/writer.java
+++ ProyectoProgramadoII_IIIPOO/src/XML/writer.java
@@ -20,14 +20,12 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import Clases.Platillo;
 
 public class writer {
     
-    
-
-	public static void main(String argv[]) {
-
-	  try {
+    public void Writer(Platillo[] platillos){
+        try {
 
 		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
@@ -41,45 +39,59 @@ public class writer {
 		
 
 		// firstname elements
-                int cont = 1;
-                while(cont < 3){
+                int cont = 0;
+                while(cont < platillos.length ){
                     
                 Element platillo = doc.createElement("Platillo");
 		rootElement.appendChild(platillo);
 
 		// shorten way
-		platillo.setAttribute("id", "BEB-"+Integer.toString(cont));
+		platillo.setAttribute("id", "BEB-"+Integer.toString(cont));//aqui falta cambiarlo para que sea el tipo de platillo correcto, 
+                                                                            //pero hasta que hagan la clase TipoPlatillo
                  
                 Element nombre = doc.createElement("nombre");
-		nombre.appendChild(doc.createTextNode("pizza"));
+		nombre.appendChild(doc.createTextNode(platillos[cont].getNombre()));
 		platillo.appendChild(nombre);
                 
 		Element descripcion = doc.createElement("descripcion");
-		descripcion.appendChild(doc.createTextNode("sabor pepperoni,muy rica"));
+		descripcion.appendChild(doc.createTextNode(platillos[cont].getDescripcion()));
 		platillo.appendChild(descripcion);
 
                 
 		Element precio = doc.createElement("precio");
-		precio.appendChild(doc.createTextNode("10000"));
+		precio.appendChild(doc.createTextNode(Double.toString(platillos[cont].getPrecio())));
 		platillo.appendChild(precio);
                 
                 Element tipoPorcion = doc.createElement("tipoPorcion");
-		tipoPorcion.appendChild(doc.createTextNode("taza"));
+		tipoPorcion.appendChild(doc.createTextNode(platillos[cont].getTipoPorcion()));
 		platillo.appendChild(tipoPorcion);
+                
+                Element habilitado = doc.createElement("Habilitado");
+		habilitado.appendChild(doc.createTextNode(Boolean.toString(platillos[cont].getHabilitado())));
+		platillo.appendChild(habilitado);
+                
+                Element cantPedidos = doc.createElement("CantPedidos");
+		cantPedidos.appendChild(doc.createTextNode(Integer.toString(platillos[cont].getCantPedidos())));
+		platillo.appendChild(cantPedidos);
+                
+                Element codClave = doc.createElement("codClave");
+		codClave.appendChild(doc.createTextNode(platillos[cont].getCodClave()));
+		platillo.appendChild(codClave);
+                
+                Element cantPlatos = doc.createElement("CantPlatos");
+		cantPlatos.appendChild(doc.createTextNode(Integer.toString(platillos[cont].getCantidadPlatos())));
+		platillo.appendChild(cantPlatos);
+                
+                
                 
                 cont++;
                 }
-                
-                
-                
-                
-
 		// write the content into xml file
 		TransformerFactory transformerFactory = TransformerFactory.newInstance();
 		Transformer transformer = transformerFactory.newTransformer();
                 transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 		DOMSource source = new DOMSource(doc);
-		StreamResult result = new StreamResult(new File("C:\\users\\erick\\Desktop\\file.xml"));
+		StreamResult result = new StreamResult(new File("C:\\users\\erick\\Desktop\\file.xml"));// aqui tienen que cambiar la direccion a la que ustedes quieran
 
 		// Output to console for testing
 		// StreamResult result = new StreamResult(System.out);
@@ -93,5 +105,103 @@ public class writer {
 	  } catch (TransformerException tfe) {
 		tfe.printStackTrace();
 	  }
-	}
+    }
+
+    public static void main(String argv[]) {
+        writer xml = new writer();
+        Platillo platillito = new Platillo();
+        platillito.setNombre("pizzaaa");
+        platillito.setPrecio(1000);
+        platillito.setDescripcion("muy rica");
+        platillito.setHabilitado(true);
+        platillito.setCodClave("BEB-001");
+        platillito.setCantPedidos(10);
+        platillito.setTipoPorcion("plato");
+        platillito.setCantidadPlatos(3);
+        Platillo[] lista = new Platillo[2];
+        lista[0]= platillito;
+        Platillo platillito2 = new Platillo();
+        platillito2.setNombre("macarrones");
+        platillito2.setPrecio(10020);
+        platillito2.setDescripcion("muy rico");
+        platillito2.setHabilitado(false);
+        platillito2.setCodClave("BEB-001");
+        platillito2.setCantPedidos(12);
+        platillito2.setTipoPorcion("plato");
+        platillito2.setCantidadPlatos(6);
+        lista[1] = platillito2;
+        System.out.println(lista[0].getNombre());
+        xml.Writer(lista);
+        
+        
+    }
 }
+
+//	  try {
+//
+//		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+//		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+//                
+//		// root elements
+//		Document doc = docBuilder.newDocument();
+//		Element rootElement = doc.createElement("Catalogo");
+//		doc.appendChild(rootElement);
+//
+//		// staff elements
+//		
+//
+//		// firstname elements
+//                int cont = 1;
+//                while(cont < 3){
+//                    
+//                Element platillo = doc.createElement("Platillo");
+//		rootElement.appendChild(platillo);
+//
+//		// shorten way
+//		platillo.setAttribute("id", "BEB-"+Integer.toString(cont));
+//                 
+//                Element nombre = doc.createElement("nombre");
+//		nombre.appendChild(doc.createTextNode("pizza"));
+//		platillo.appendChild(nombre);
+//                
+//		Element descripcion = doc.createElement("descripcion");
+//		descripcion.appendChild(doc.createTextNode("sabor pepperoni,muy rica"));
+//		platillo.appendChild(descripcion);
+//
+//                
+//		Element precio = doc.createElement("precio");
+//		precio.appendChild(doc.createTextNode("10000"));
+//		platillo.appendChild(precio);
+//                
+//                Element tipoPorcion = doc.createElement("tipoPorcion");
+//		tipoPorcion.appendChild(doc.createTextNode("taza"));
+//		platillo.appendChild(tipoPorcion);
+//                
+//                cont++;
+//                }
+//                
+//                
+//                
+//                
+//
+//		// write the content into xml file
+//		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+//		Transformer transformer = transformerFactory.newTransformer();
+//                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+//		DOMSource source = new DOMSource(doc);
+//		StreamResult result = new StreamResult(new File("C:\\users\\erick\\Desktop\\file.xml"));
+//
+//		// Output to console for testing
+//		// StreamResult result = new StreamResult(System.out);
+//
+//		transformer.transform(source, result);
+//
+//		System.out.println("File saved!");
+//
+//	  } catch (ParserConfigurationException pce) {
+//		pce.printStackTrace();
+//	  } catch (TransformerException tfe) {
+//		tfe.printStackTrace();
+//	  }
+//	}
+//}
