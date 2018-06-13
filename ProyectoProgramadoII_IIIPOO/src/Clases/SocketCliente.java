43954be2e9206e2aa35332f3e9464c0db9bdc740
erick98 <ericksalazar_g@hotmail.com>
2018-06-12 23:04:33 -0600

    Auto stash before merge of "master" and "origin/master"

--- ProyectoProgramadoII_IIIPOO/src/Clases/SocketCliente.java
+++ ProyectoProgramadoII_IIIPOO/src/Clases/SocketCliente.java
@@ -8,19 +8,49 @@ import java.io.*;
 
 
 public class SocketCliente {
-
-    public static void main(String args[])throws IOException , ClassNotFoundException,UnknownHostException {
+    
+    public void generarOrden(Platillo[] listaPlatillos)throws IOException , ClassNotFoundException,UnknownHostException{
+        Socket socket = new Socket("localhost",SocketServidor.PORT);//conecta al mismo puerto del servidor
+        System.out.println("Conectado");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());//devuelve flujo
+        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());//recibe flujo
+        System.out.println("listo para enviar");
+        objectOutputStream.writeObject("generarOrden");
+        System.out.println("enviado");
+        objectOutputStream.writeObject(listaPlatillos);
+        System.out.println("enviado");
+        Platillo[] resultado = (Platillo[])objectInputStream.readObject();//el  server me devuelve el objeto para ver si esta sano
+        System.out.println("Recibido de Cliente:"+resultado[0].getNombre());//me imprime un string modificado en el server
+        socket.close();
+    }
+    
+    public void funcionalidades(String funcionalidad)throws IOException , ClassNotFoundException,UnknownHostException{
         Socket socket = new Socket("localhost",SocketServidor.PORT);//conecta al mismo puerto del servidor
         System.out.println("Conectado");
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());//devuelve flujo
         ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());//recibe flujo
         Platillo pollo = new Platillo("Pollo",5000,100);    //Cliente crea objeto \
         System.out.println("listo para enviar");
-        objectOutputStream.writeObject(pollo);
+        objectOutputStream.writeObject(funcionalidad);
         System.out.println("enviado");
-        Platillo resultado = (Platillo)objectInputStream.readObject();//el  server me devuelve el objeto para ver si esta sano
-        System.out.println("Recibido de Cliente:"+resultado.getNombre());//me imprime un string modificado en el server
+        Platillo[] lista = new Platillo[100];//creo una lista que tiene un platillo para que el servidor no de problemas, pero no se usa para nada
+        lista[0] = pollo;
+        objectOutputStream.writeObject(lista);
+        System.out.println("enviado");
+        //Platillo[] resultado = (Platillo[])objectInputStream.readObject();//el  server me devuelve el objeto para ver si esta sano
+        //System.out.println("Recibido de Cliente:"+resultado[0].getNombre());//me imprime un string modificado en el server
         socket.close();
+    }
+        
+
+    public static void main(String args[])throws IOException , ClassNotFoundException,UnknownHostException {
+        SocketCliente nuevaConexion = new SocketCliente();
+        Platillo[] listaPlatillos = new Platillo[100];
+        Platillo p1 = new Platillo("pollo",1000,200);
+        Platillo p2 = new Platillo("Bistec",1500,400);
+        listaPlatillos[0]= p1;
+        listaPlatillos[1]= p2;
+        nuevaConexion.funcionalidades("borrar");
     }
 
     
--- ProyectoProgramadoII_IIIPOO/src/Clases/SocketServidor.java
+++ ProyectoProgramadoII_IIIPOO/src/Clases/SocketServidor.java
@@ -16,7 +16,7 @@ public class SocketServidor {
 
 	 
     public static boolean state = true;
-    public static final int PORT = 8080;        //Puerto de conexion
+    public static final int PORT = 4444;        //Puerto de conexion
     public static Socket socket;
     public static void main(String args[]) throws IOException , ClassNotFoundException{
         new SocketServidor().runServer();   //ejecuta el server
@@ -26,8 +26,8 @@ public class SocketServidor {
         ServerSocket serverSocket = new ServerSocket(PORT); //conecta al puerto
         System.out.println("Listo para conexiones...");
         
-        Platillo plato;
-        ((ServerCloser) new ServerCloser()).start();
+        Platillo[] plato;
+        String funcion;
         
         while (state){ 
             socket = serverSocket.accept();//acepta conexion
@@ -35,15 +35,28 @@ public class SocketServidor {
             ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());   // capta flujo de datos
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());   //devuelve flujo de datos
             try{
-                plato = (Platillo) objectInputStream.readObject();
-            if (plato!= null){
-                System.out.println("holaaa");
-                Platillo pollo = plato; //Dado el flujo recibido, recibe un  objeto, en este caso es el Platillo que yo se que lo envia
-                System.out.println("1");
-                probar(pollo); //pruebo si sirve                            //sino habria que captar entradas de forma mas general para castearlo.
-                System.out.println("2");
-                objectOutputStream.writeObject(pollo);
-                System.out.println("3");
+                funcion = (String) objectInputStream.readObject();
+            if (funcion!= null){
+                if(funcion.equals("borrar")){
+                    plato = (Platillo[]) objectInputStream.readObject();//esto es solo para que lea algo, aunque realmente no hace nada
+                    System.out.println("Se borro con exito");
+                }
+                if (funcion.equals("generarOrden")){
+                    plato = (Platillo[]) objectInputStream.readObject();
+                    Platillo[] pollo = plato; //Dado el flujo recibido, recibe un  objeto, en este caso es el Platillo que yo se que lo envia
+                
+                    probar(pollo); //pruebo si sirve                            //sino habria que captar entradas de forma mas general para castearlo.
+                
+                    objectOutputStream.writeObject(pollo);
+                    System.out.println("se genero la orden con exito");
+                }
+                if(funcion.equals("cerrar")){
+                    plato = (Platillo[]) objectInputStream.readObject();//esto es solo para que lea algo, aunque realmente no hace nada
+                    SocketServidor.state = false;
+                    System.out.println("se cerro el servidor");
+                }
+                
+                
                 socket.close();
             }          
             }
@@ -55,8 +68,8 @@ public class SocketServidor {
 
     }
     
-    private void probar(Platillo p){
-        p.setSocket(p.nombre);      //devuelve nombre de platillo dado por cliente
+    private void probar(Platillo[] p){
+        p[0].setSocket(p[0].nombre);      //devuelve nombre de platillo dado por cliente
         
     }
     
