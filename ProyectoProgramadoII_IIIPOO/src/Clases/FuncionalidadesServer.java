ac54d4e43f47a2fb28f0e67ecfcbc1074c171f1c
Dereck <dereckescalante@hotmail.com>
2018-06-15 00:14:10 -0600

    Top 10 y top 0 listos

--- ProyectoProgramadoII_IIIPOO/src/Clases/FuncionalidadesServer.java
+++ ProyectoProgramadoII_IIIPOO/src/Clases/FuncionalidadesServer.java
@@ -57,24 +57,98 @@ public class FuncionalidadesServer {
         int size = listaPlatillos.length;
         while(cont<size){
             Platillo plVector = listaPlatillos[cont];
-            if (pl.equals(plVector))
+            if (pl.nombre.equals(plVector.nombre)){
                    resul = cont; 
-            else
+                   break;
+            }
+            else{
                 cont++;
+            }
         }
         return resul;
         
     }
     
+    public String topTen(){
+        int contador = 0;
+        int contTop = 0;
+        int cantMasAlta=0;
+        int place = 1;
+        Platillo[] aux = listaPlatillos;
+        String resul = "";
+        
+        while(listaPlatillos[contador]!=null){
+            Platillo platillo = listaPlatillos[contador];
+            if(platillo.cantPedidos>cantMasAlta){
+                cantMasAlta = platillo.cantPedidos;
+                contador++;
+            }
+            else{
+                contador++;
+            }
+        }
+        contador = 0;
+        resul+="Top 10:\n";
+        while(contTop<10 && contTop<arrSize(aux)){
+            while(contador<arrSize(aux)){
+                Platillo pl = aux[contador];
+                if(pl.cantPedidos == cantMasAlta){
+                    resul+=String.valueOf(place)+". "+pl.getNombre()+"("+pl.getCodClave()+")"+":"+pl.getDescripcion()+"\n";
+                    contTop+=1;
+                    contador++;
+                    place++;
+                    
+                }
+                else{
+                    contador++;
+                }
+            }
+            contador = 0;
+            cantMasAlta--;
+            
+            
+        }
+        return resul;
+    }
+    public int arrSize(Platillo[]p){
+        int cont=0;
+        while(p[cont]!=null){
+            cont++;
+        }
+        return cont;
+    }
+    
+    public String topZero(){
+        int cont = 0;
+        String resul = "";
+        int posicion =1;
+        resul+="Top 0:\n";
+        while(listaPlatillos[cont]!= null){
+            Platillo p = listaPlatillos[cont];
+            if(p.cantPedidos == 0){
+                
+                resul+=String.valueOf(posicion)+". "+ p.getNombre()+"("+p.getCodClave()+")"+"\n";
+                posicion++;
+                cont++;
+            }
+            else{
+                cont++;
+            }
+        }
+        return resul;
+        
+    }
+
+    
     
     public static void main(String args[]){
         
         FuncionalidadesServer func = new FuncionalidadesServer();   //Simulacion del reader del XML parser
-        Platillo p1 = new Platillo("pollo",1000,200);
-        Platillo p2 = new Platillo("Bistec",1500,400);
-        Platillo p3 = new Platillo("ensalada",1800,75);
-        Platillo p4 = new Platillo("Crema de Hongos",2300,300);
-        Platillo p5 = new Platillo("Casado Completo",3000,1000);
+        Platillo p1 = new Platillo("pollo",1000,200,0,"Pollo rico","PRN");
+        Platillo p2 = new Platillo("Bistec",1500,400,0,"Bistec grande","PRN");
+        Platillo p3 = new Platillo("ensalada",1800,75,0,"Ensalada Saludable","PRN");
+        Platillo p4 = new Platillo("Crema de Hongos",2300,300,0,"Sopa deliciosa","PRN");
+        Platillo p5 = new Platillo("Casado Completo",3000,1000,0,"Almuerzo de lujo","PRN");
         listaPlatillos[0]= p1;
         listaPlatillos[1]= p2;
         listaPlatillos[2]= p3;
@@ -107,8 +181,11 @@ public class FuncionalidadesServer {
         
         func.procesarPedido(pedidoTotal);
         
-        System.out.println("precio final:"+func.getPrecioTotal());
-        System.out.println("Calorias:"+func.getCaloriasTotal());
+        //System.out.println("precio final:"+func.getPrecioTotal());
+        //System.out.println("Calorias:"+func.getCaloriasTotal());
+        
+        System.out.println(func.topZero());
+        System.out.print(func.topTen());
         
         
         
